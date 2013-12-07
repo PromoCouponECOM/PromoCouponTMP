892431e7e55f4e6af8e17fe050548981e9a317e8
anthony <anthony.broguy@gmail.com>
2013-12-07 19:10:15 +0100

    mise a jour interface

--- ECOM/ECOM-war/web/js/lieu.js
+++ ECOM/ECOM-war/web/js/lieu.js
@@ -1,16 +1,5 @@
 function swichNameLieu(lieu, listeVille) {
 	document.getElementById('nameLieu').innerHTML = lieu;
 	listeVille = document.getElementById(listeVille);
-	listeVille.style.visibility = "hidden";
 	return false;
-}
-
-function toggleVisibility(listeVille)
-{
-   if(typeof listeVille == "string")
-      listeVille = document.getElementById(listeVille);
-   if(listeVille.style.visibility == "hidden")
-      listeVille.style.visibility = "visible";
-   else
-      listeVille.style.visibility = "hidden";
 }
