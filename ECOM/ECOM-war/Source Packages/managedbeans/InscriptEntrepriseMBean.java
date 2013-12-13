5dd7727b955b5268b6d9a8ab3022553973180ae1
John624 <John624@Jiang>
2013-12-13 10:52:25 +0100

    ajouter la package entities et session

---
+++ ECOM/ECOM-war/Source Packages/managedbeans/InscriptEntrepriseMBean.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import entities.Adresse;
+import entities.Entreprise;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.PersistenceContext;
+import session.AdresseManager;
+import session.EntrepriseManager;
+
+/**
+ *
+ * @author John624
+ */
+
+@Named(value = "inscripEntrepriseMBean")
+@SessionScoped
+public class InscriptEntrepriseMBean implements Serializable {
+
+    @PersistenceContext(unitName = "ECOM-ejbPU")
+    private Map<String, String> settings;
+    @EJB
+    private AdresseManager adrM;
+    @EJB
+    private EntrepriseManager entM;
+    
+    private Adresse adr;
+    private Entreprise entrp;
+            
+    public InscriptEntrepriseMBean() {
+        settings= new HashMap<String,String>();
+        adr = new Adresse();
+        entrp = new Entreprise();
+    }
+    
+    
+    public void init (){
+        adrM = new AdresseManager();
+        entM = new EntrepriseManager();
+    }
+    
+    
+    public void save(){
+        
+        //update db adresse
+        adr.setIdAdresse(Long.parseLong(settings.get("idAdr")));
+        adr.setNumEtRue(settings.get("rue"));
+        adr.setComple(settings.get("compl"));
+        adr.setVille(settings.get("ville"));
+        adr.setPays(settings.get("pays"));
+        adr.setCodePostale(settings.get("code"));
+        adr.setDateModif(new Date());
+        adrM.update(adr);
+        
+        //
+        //in=new Integer(settings.get("idEnp"));
+        entrp.setIdE(Integer.parseInt(settings.get("idEnp")));
+        entrp.setMailE(settings.get("mailE"));
+        entrp.setNomE(settings.get("nomE"));
+        entrp.setPassE(settings.get("passE"));
+        entrp.setResuE(settings.get("resuE"));
+        entrp.setTelE(settings.get("telE"));
+        entrp.setConsulE(settings.get("consulE"));
+        entrp.setRib(settings.get("rib"));
+        entM.update(entrp);
+    }
+    
+     public Map<String, String> getSettings() {
+        return settings;
+    }
+}
